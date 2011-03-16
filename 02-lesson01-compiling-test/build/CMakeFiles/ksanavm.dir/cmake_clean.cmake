db7faf37a5a51c90e58ad5d34cfa58948bb618e3
Hu Songtao <idisblueflash@gmail.com>
2011-03-16 16:52:32 +0800

    创建build目录

---
+++ 02-lesson01-compiling-test/build/CMakeFiles/ksanavm.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ksanavm.dir/lesson1.c.o"
+  "CMakeFiles/ksanavm.dir/lesson3.c.o"
+  "CMakeFiles/ksanavm.dir/lesson4.c.o"
+  "CMakeFiles/ksanavm.dir/main.c.o"
+  "CMakeFiles/ksanavm.dir/lesson2.c.o"
+  "CMakeFiles/ksanavm.dir/vm.c.o"
+  "ksanavm.pdb"
+  "ksanavm"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/ksanavm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
