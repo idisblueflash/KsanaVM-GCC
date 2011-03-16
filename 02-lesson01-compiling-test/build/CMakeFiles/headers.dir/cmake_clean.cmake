0ba6027e2addc04d54c7b5ca805e1acd75aa825c
Hu Songtao <idisblueflash@gmail.com>
2011-03-16 17:21:47 +0800

    最后一次cmake ..;make 尝试

---
+++ 02-lesson01-compiling-test/build/CMakeFiles/headers.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/headers.dir/main.c.o"
+  "libheaders.pdb"
+  "libheaders.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/headers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
